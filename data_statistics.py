f28bd48d32ab5b183a5981679c345a9311463524
Articha <tima-1324@mail.ru>
2024-02-06 23:29:18 +0300

    data_statistics.py refactoring

--- data_statistics.py
+++ data_statistics.py
@@ -12,13 +12,16 @@ __all__ = (
 
 
 def calculate_line(df: pd.DataFrame):
+    print('- - Calculating lines')
     all_diagnoses = df['diagnose'].unique()
+    print('Column', end='')
     for name in df.columns:
         if any((
             name in {'sex', 'age', 'diagnose'},
             '/' in name
         )):
             continue
+        print(f' {name}', end='')
         df.sort_values(name, inplace=True, ignore_index=True)
         f, ax = plt.subplots(figsize=(19.20, 10.80))
         diagnoses = {i: ([], []) for i in all_diagnoses}
@@ -37,9 +40,11 @@ def calculate_line(df: pd.DataFrame):
         plt.legend(all_diagnoses)
         plt.savefig(f"graphs\\line\\{name}.png")
         plt.close('all')
+    print()
 
 
 def calculate_diagnose_bars(df: pd.DataFrame) -> pd.Series:
+    print('- - Calculating diagnose bars')
     plt.close()
     ax = plt.subplot()
     values = df['diagnose'].value_counts()
@@ -48,25 +53,30 @@ def calculate_diagnose_bars(df: pd.DataFrame) -> pd.Series:
     ax.set_xlabel('Диагноз')
     plt.setp(ax.get_xticklabels(), rotation=10, ha='right')
     plt.tight_layout()
-    plt.show()
+    plt.savefig('graphs\\diagnose_bars.png')
     return values
 
 
 def calculate_density(df: pd.DataFrame):
+    raise NotImplementedError()
+    print('- - Calculating density bars')
+    print('Column', end='')
     for name in df.columns:
+        print(f' {name}', end='')
         ax = df[name].plot(kind='kde')
         ax: plt.Axes
         ax.set_title(name)
         name = name.replace(' ', '_').replace('/', '_')
         plt.savefig(f"graphs\\density\\{name}.png")
         plt.close('all')
+    print()
 
 
 def main():
     df = load_info()
-    # calculate_diagnose_bars(df)
+    calculate_diagnose_bars(df)
     calculate_line(df)
-    # print(df.info())
+    # calculate_density(df)
 
 
 if __name__ == '__main__':
